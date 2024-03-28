d1dfa211df5324f3047b4e0c7f8c5f01a77c82d1
Constantine Kuletsky <kuletsky@gmail.com>
2024-03-28 14:23:00 -0400

    add

--- sync.py
+++ sync.py
@@ -1,1 +1,9 @@
 from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://target.com")
+    page.screenshot(path="demo.png")
+    browser.close()
+
